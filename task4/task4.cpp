593579fa040ff03e0b9dc454e48e6d99927a51e6
gh6u9do <thedovakin31@mail.ru>
2021-11-26 19:48:49 +0300

    задание 4, 12 месяцев

---
+++ task4/task4.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(0, "");
+
+	int number;
+	cout << "[+] Месяца года\n[1]Январь\n[2]Февраль\n[3]Март\n[4]Апрель\n[5]Май\n[6]Июнь" << endl;
+	cout << "[7]Июль\n[8]Август\n[9]Сентябрь\n[10]Октябрь\n[11]Ноябрь\n[12]Декабрь\n";
+	cout << "Введите значение:";
+	cin >> number;
+
+	switch (number) {
+	case 1:
+		cout << "Январь - пьянварь";
+		break;
+	case 2:
+		cout << "Февраль - Холодно ";
+		break;
+	case 3:
+		cout << "Март-Теплеет";
+		break;
+	case 4:
+		cout << "Апрель- А у вас тема светлая";
+		break;
+	case 5:
+		cout << "Мир труд май";
+		break;
+	case 6:
+		cout << "Июнь-Жарунь";
+		break;
+	case 7:
+		cout << "Июль-Жарюль";
+		break;
+	case 8:
+		cout << "Август- Последний месяц чила";
+		break;
+	case 9:
+		cout << "Сентябрь- я календарь переверну и снова 3 сентября ";
+		break;
+	case 10:
+		cout << "Октябрь - котябрь";
+		break;
+	case 11:
+		cout << "Ноябрь - Да ";
+		break;
+	case 12:
+		cout << "Декабрь - Возвращение дубака";
+		break;
+	default:
+		cout << "тебе же сказали, от 1 до 12, цыфры в школе не учил?";
+	}
+
+	return 0;
+}
