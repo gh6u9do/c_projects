e31336bb710e62fe4c934a589316de37936ed751
gh6u9do <thedovakin31@mail.ru>
2021-11-26 19:41:41 +0300

    добавлено 2 задание (времена года)

---
+++ second_task/second_task.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    cout << "1 - Лето\n2 - Осень\n3 - Зима\n4 - Весна\n";
+    int season;
+    cin >> season;
+    if (season == 1) {
+        cout << "Сейчас лето - жарко ";
+    }
+    else if (season == 2) {
+        cout << "Сейчас осень, холодно и дождливо -сиди дома";
+    }
+    else if (season == 3) {
+        cout << "Сейчас зима, красиво, но холодно";
+    }
+    else if (season == 4) {
+        cout << "Сейчас весна, надевай шапку, еще не лето";
+    }
+    else {
+        cout << "Вы ввели не то";
+    }
+    return 0;
+}
